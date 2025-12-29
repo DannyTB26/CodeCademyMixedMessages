e7f856e133e783387903c50e272d96b5bafd11f8
DannyTB26 <DannyJBaylan@outlook.com>
2025-12-29 08:04:27 +0000

    Added algorithm to calculate the number code

---
+++ src/main.js
@@ -0,0 +1,40 @@
+function getInput() {
+  let userInput = "test"; //prompt('Enter a message: ', null);
+
+  getOutput(algorithm(userInput));
+}
+
+function getOutput(messageCode) {
+
+  if (messageCode == null) {
+    console.log("You didn't enter a value.")
+  } else {
+    console.log("You entered: ", messageCode.toString());
+  }
+}
+
+function algorithm(userInput) {
+  let length = userInput.length;
+
+  let count = 0;
+
+  for (let i = 0; i < length; i++) {
+    if (userInput[i] == ' ') {
+      continue;
+    } else {
+      count += userInput.charCodeAt(i);
+    }
+  }
+
+  count /= length;
+
+  count %= 26;
+
+  return Math.floor(count);
+}
+
+const messages = {
+  numberId:0,
+}
+
+getInput();
